53da47326f0bc46d39241a2165bfa4f5d8d1bd81
AroPho <aaronnguyen2009@yahoo.com>
2019-05-02 00:12:12 -0700

    Combining the branches and adding PA3

---
+++ PA3/src/List.java
@@ -0,0 +1,315 @@
+import java.lang.String;
+import java.lang.IndexOutOfBoundsException;
+
+public class List {
+    private class Node{
+        Object matrix;
+        Node next;
+        Node prev;
+
+        Node(Object x){
+            matrix = x;
+            next = null;
+            prev = null;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private Node cursor;
+    private int cursorInd;
+    private int numItems;
+
+    public List(){
+        head = tail = cursor =null;
+        cursorInd = -1;
+        numItems = 0;
+    }
+
+    public int length(){
+        return numItems;
+    }
+
+    public int index(){
+        return cursorInd;
+    }
+
+    public Object front() throws IndexOutOfBoundsException{
+        if(numItems > 0) {
+            return head.matrix;
+        }else{
+            throw new IndexOutOfBoundsException("list is empty");
+        }
+    }
+
+    public Object back(){
+        if(numItems > 0) {
+            return tail.matrix;
+        }else{
+            throw new IndexOutOfBoundsException("list is empty");
+        }
+    }
+
+    public Object get(){
+        if(numItems > 0 && cursorInd >= 0 && cursorInd < numItems ) {
+            return cursor.matrix;
+        }else{
+            return -1;
+        }
+    }
+
+    public boolean equals(List L){
+        if(numItems != L.numItems){
+            return false;
+        }else{
+            Node temp = head;
+            Node tempL = L.head;
+            for(int x = 0; x < numItems; x++){
+                if(temp.matrix != tempL.matrix){
+                    return false;
+                }else{
+                    temp = temp.next;
+                    tempL = tempL.next;
+                }
+            }
+            return true;
+        }
+    }
+    public void clear(){
+        head = tail = cursor = null;
+        numItems = 0;
+        cursorInd = -1;
+    }
+    public void moveFront() throws IndexOutOfBoundsException{
+        if(numItems == 0){
+            //throw new IndexOutOfBoundsException("List is Empty");
+        }else{
+            cursor = head;
+            cursorInd = 0;
+        }
+    }
+
+    public void moveBack(){
+        if(numItems == 0){
+            //throw new IndexOutOfBoundsException("List is Empty");
+        }else{
+            cursor = tail;
+            cursorInd = numItems - 1;
+        }
+    }
+
+    public void movePrev(){
+        if(cursorInd != -1 && cursor != head){
+            cursor = cursor.prev;
+            cursorInd--;
+        }else if(cursor == head){
+            cursor = null;
+            cursorInd = -1;
+        }
+    }
+
+    public void moveNext(){
+        if(cursor != null && cursor != tail){
+            cursor = cursor.next;
+            cursorInd++;
+        }else if(cursor == tail){
+            cursor = null;
+            cursorInd = -1;
+        }
+    }
+
+    public void prepend(Object data){
+        if(numItems == 0){
+            head = tail = new Node(data);
+            numItems++;
+        }else{
+            Node temp = head;
+            head = new Node(data);
+            head.next = temp;
+            temp.prev = head;
+            numItems++;
+            if(cursorInd != -1 ){
+                cursorInd++;
+            }
+        }
+    }
+
+    public void append(Object data){
+        if(numItems == 0){
+            head = new Node(data);
+            tail = head;
+            numItems++;
+        }else{
+            tail.next = new Node(data);
+            tail.next.prev = tail;
+            tail = tail.next;
+            numItems++;
+        }
+    }
+
+    public void insertBefore(Object data){
+        if(numItems !=0 && cursorInd >= 0){
+            if(cursorInd == 0){
+                prepend(data);
+            }else{
+                Node A = cursor.prev;
+                Node temp = new Node(data);
+                A.next = temp;
+                temp.prev = A;
+                temp.next = cursor;
+                cursor.prev = temp;
+                cursorInd++;
+                numItems++;
+            }
+        }
+    }
+
+    public void insertAfter(Object data){
+        if(numItems !=0 && cursorInd >= 0){
+            if(cursor == tail){
+                append(data);
+            }else{
+                Node A = cursor.next;
+                Node temp = new Node(data);
+                A.prev = temp;
+                temp.next = A;
+                temp.prev = cursor;
+                cursor.next = temp;
+                numItems++;
+            }
+        }
+    }
+
+    public void deleteFront(){
+        if(numItems != 0){
+            head = head.next;
+            head.prev = null;
+            numItems--;
+            if(cursorInd == 0){
+                cursorInd--;
+                cursor = null;
+            }else if(cursorInd != -1 ){
+                cursorInd--;
+            }
+        }
+    }
+
+    public void deleteBack(){
+        if(numItems > 0){
+            if (cursor == tail){
+                cursor = null;
+                cursorInd = -1;
+            }
+            if(numItems == 1){
+                tail = null;
+                head = null;
+                numItems--;
+            }else {
+                tail = tail.prev;
+                tail.next = null;
+                numItems--;
+            }
+        }
+    }
+    void delete(){
+        if(cursorInd == 0 && numItems > 0){
+            deleteFront();
+        }else if(cursorInd == numItems - 1 && numItems > 0){
+            deleteBack();
+        }else if(cursorInd >= 0 && numItems != 0){
+            Node curprev  = cursor.prev;
+            Node curnext = cursor.next;
+            curprev.next = curnext;
+            curnext.prev = curprev;
+            numItems--;
+            cursorInd = -1;
+            cursor = null;
+        }
+    }
+    private String myString(Node H){
+        if( H==null ){
+            return "";
+        }else{
+            return (H.matrix+ " " +myString(H.next));
+        }
+    }
+
+    public String toString(){
+        return myString(head);
+    }
+
+    /*public List copy(){
+        List A = new List();
+        Node temp = head;
+        while(temp != null){
+            A.append(temp.matrix);
+            temp = temp.next;
+        }
+        return A;
+    }*/
+
+    /*public List concat(List L){
+        List A = new List();
+        Node temp = L.head;
+        A = copy();
+        while(temp != null){
+            A.append(temp.data);
+            temp = temp.next;
+        }
+        return A;
+    }*/
+    /*public static void main(String[] args){
+        List A = new List();
+        List B = new List();
+
+        for(int i=1; i<=20; i++){
+            A.append(i);
+            B.prepend(i);
+        }
+        System.out.println(A);
+        System.out.println(B);
+
+        for(A.moveFront(); A.index()>=0; A.moveNext()){
+            System.out.print(A.get()+" ");
+        }
+        System.out.println();
+        for(B.moveBack(); B.index()>=0; B.movePrev()){
+            System.out.print(B.get()+" ");
+        }
+        System.out.println();
+
+        List C = A.copy();
+        System.out.println(A.equals(B));
+        System.out.println(B.equals(C));
+        System.out.println(C.equals(A));
+        System.out.println(C);
+        System.out.println(A);
+
+        A.moveFront();
+        for(int i=0; i<5; i++) A.moveNext(); // at index 5
+        A.insertBefore(-1);                  // at index 6
+        for(int i=0; i<9; i++) A.moveNext(); // at index 15
+        A.insertAfter(-2);
+        for(int i=0; i<5; i++) A.movePrev(); // at index 10
+        A.delete();
+        System.out.println(A);
+        System.out.println(A.length());
+        A.deleteBack();
+        System.out.println(A);
+        A.clear();
+        System.out.println(A.length());
+    }*/
+}
+
+// Output of this program:
+// 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+// 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1
+// 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+// 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+// false
+// false
+// true
+// 1 2 3 4 5 -1 6 7 8 9 11 12 13 14 15 -2 16 17 18 19 20
+// 21
+// 0
+
---
+++ PA3/src/Matrix.java
@@ -0,0 +1,219 @@
+import java.io.*;
+
+
+public class Matrix {
+
+    private class Point{
+        int column;
+        double ent;
+
+        Point(int c, double x){
+            column = c;
+            ent = x;
+        }
+        public boolean equals(Point e){
+            if( column != e.column){
+                return false;
+            }else{
+                return true;
+                /*if(ent == e.ent) {
+                    return true;
+                }return false;*/
+            }
+        }
+        private String toString(Point e){
+            if( e==null ){
+                return "";
+            }else{
+                return ("(" + e.column + ", " + e.ent + ")");
+            }
+        }
+
+    }
+
+    private int size;
+    private int nonzeros;
+    private List[] matrix;
+
+    // Constructor
+    Matrix(int n){ // Makes a new n x n zero Matrix. pre: n>=1
+        if(n > 0) {
+            List[] matrix = new List[n];
+            size = n;
+            nonzeros = 0;
+        }
+    }
+
+    // Access functions
+    int getSize() {// Returns n, the number of rows and columns of this Matrix
+        return size;
+    }
+    int getNNZ(){ // Returns the number of non-zero entries in this Matrix
+        return nonzeros;
+    }
+
+    public boolean equals(Object x){
+        return false;
+    } // overrides Object's equals() method
+
+    // Manipulation procedures
+
+    void makeZero(){
+        for(int x = 0; x < size; x++){
+            matrix[x].clear();
+        }
+    } // sets this Matrix to the zero state
+
+    Matrix copy(){
+        Matrix A = new Matrix(size);
+        for(int x = 0; x < size; x++){
+            matrix[x].moveFront();
+            while(matrix[x].index() != -1){
+                Point temp = (Point) matrix[x].get();
+                A.changeEntry(x, temp.column, temp.ent);
+                matrix[x].moveNext();
+            }
+        }
+        return A;
+    }// returns a new Matrix having the same entries as this Matrix
+
+    void changeEntry(int i, int j, double x){
+        Point temp = new Point(j, x);
+        if(matrix[i].length() == 0 && x != 0){
+            matrix[i].append(temp);
+            nonzeros++;
+        }else {
+            matrix[i].moveFront();
+            while (matrix[i].index() < i) {
+                matrix[i].moveNext();
+            }
+            if (matrix[i].index() == -1) {
+                matrix[i].prepend(temp);
+                nonzeros++;
+            }else if(matrix[i].index() == i){
+                if(x == 0){
+                    matrix[i].delete();
+                    nonzeros--;
+                }else if(matrix[i].get() == temp){
+                    return;
+                }else{
+                    matrix[i].insertAfter(temp);
+                    matrix[i].delete();
+                    nonzeros++;
+                }
+            }else{
+                if(x != 0) {
+                    matrix[i].insertBefore(temp);
+                    nonzeros++;
+                }
+            }
+        }
+    }
+    // changes ith row, jth column of this Matrix to x
+    // pre: 1<=i<=getSize(), 1<=j<=getSize()
+
+    Matrix scalarMult(double x){
+        Matrix A = new Matrix(size);
+        for(int i = 0; i < size;i++){
+            List Ltemp = A.matrix[i];
+            Point temp;
+            Ltemp.moveFront();
+            while(Ltemp.get() != null){
+                temp = (Point) Ltemp.get();
+                changeEntry(i, temp.column, temp.ent * x );
+                Ltemp.moveNext();
+            }
+        }
+        return A;
+    }
+
+    // returns a new Matrix that is the scalar product of this Matrix with x
+
+    Matrix add(Matrix M){
+        Matrix A = new Matrix(size);
+        for(int i = 0; i < size;i++){
+            List a = matrix[i];
+            List b = M.matrix[i];
+            a.moveFront();
+            b.moveFront();
+            Point aent = (Point) a.get();
+            Point bent = (Point) b.get();
+            int count = 1;
+            while(count <= size){
+                if(aent.equals(bent) && aent.column == count){
+                    A.changeEntry(i, count, aent.ent + bent.ent);
+                    a.moveNext();
+                    b.moveNext();
+                }else if(aent.column != count && bent.column != count){
+                    a.moveNext();
+                    b.moveNext();
+                }else if(aent.column == count){
+                    A.changeEntry(i, count, aent.ent);
+                    a.moveNext();
+                }else{
+                    A.changeEntry(i, count, bent.ent);
+                    b.moveNext();
+                }
+                count++;
+            }
+        }
+        return A;
+
+    }
+    // returns a new Matrix that is the sum of this Matrix with M
+    // pre: getSize()==M.getSize()
+
+    Matrix sub(Matrix M){
+        Matrix A = new Matrix(size);
+        for(int i = 0; i < size;i++){
+            List a = matrix[i];
+            List b = M.matrix[i];
+            a.moveFront();
+            b.moveFront();
+            Point aent = (Point) a.get();
+            Point bent = (Point) b.get();
+            int count = 1;
+            while(count <= size){
+                if(aent.equals(bent) && aent.column == count){
+                    A.changeEntry(i, count, aent.ent - bent.ent);
+                    a.moveNext();
+                    b.moveNext();
+                }else if(aent.column != count && bent.column != count){
+                    a.moveNext();
+                    b.moveNext();
+                }else if(aent.column == count){
+                    A.changeEntry(i, count, aent.ent);
+                    a.moveNext();
+                }else{
+                    A.changeEntry(i, count, -bent.ent);
+                    b.moveNext();
+                }
+                count++;
+            }
+        }
+        return A;
+    }
+
+    // returns a new Matrix that is the difference of this Matrix with M
+    // pre: getSize()==M.getSize()
+    Matrix transpose(){
+        return this;
+    }
+    // returns a new Matrix that is the transpose of this Matrix
+
+    Matrix mult(Matrix M){
+        return this;
+    }
+    // returns a new Matrix that is the product of this Matrix with M
+    // pre: getSize()==M.getSize()
+
+    // Other functions
+    public String toString(){
+        return "fuck";
+    } // overrides Object's toString() method
+
+
+
+
+
+}
