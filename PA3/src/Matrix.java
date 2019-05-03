4fa822c81c9540fde21423f7af672eb989f99899
AroPho <aaronnguyen2009@yahoo.com>
2019-05-02 21:10:28 -0700

    fixed pa3 add/sub and started mult

--- PA3/src/Matrix.java
+++ PA3/src/Matrix.java
@@ -56,6 +56,30 @@ public class Matrix {
         return false;
     } // overrides Object's equals() method
 
+    public int dot(Matrix A, Matrix B, int r, int c){
+        double[] product = new double[size];
+        int sum;
+        Point aent, bent;
+        List a = A.matrix[r];
+        List b = B.matrix[0];
+        a.moveFront();
+        b.moveFront();
+        int count = 1;
+        while(count <= 3){
+            aent = (Point) a.get();
+            bent = (Point) b.get();
+            if(aent.column == count && bent.column == c){
+                product[count - 1] = aent.ent * bent.ent;
+                count++;
+                a.moveNext();
+            }
+
+
+        }
+
+
+    }
+
     // Manipulation procedures
 
     void makeZero(){
@@ -129,36 +153,50 @@ public class Matrix {
 
     // returns a new Matrix that is the scalar product of this Matrix with x
 
-    Matrix add(Matrix M){
+    Matrix add(Matrix M) {
         Matrix A = new Matrix(size);
-        for(int i = 0; i < size;i++){
+        for (int i = 0; i < size; i++) {
             List a = matrix[i];
             List b = M.matrix[i];
             a.moveFront();
             b.moveFront();
-            Point aent = (Point) a.get();
-            Point bent = (Point) b.get();
+            Point aent, bent;
             int count = 1;
-            while(count <= size){
-                if(aent.equals(bent) && aent.column == count){
-                    A.changeEntry(i, count, aent.ent + bent.ent);
-                    a.moveNext();
-                    b.moveNext();
-                }else if(aent.column != count && bent.column != count){
-                    a.moveNext();
-                    b.moveNext();
-                }else if(aent.column == count){
-                    A.changeEntry(i, count, aent.ent);
-                    a.moveNext();
-                }else{
-                    A.changeEntry(i, count, bent.ent);
-                    b.moveNext();
+            while (count <= size) {
+                if(a.index() != -1 && b.index() != -1) {
+                    aent = (Point) a.get();
+                    bent = (Point) b.get();
+                    if (aent.equals(bent) && aent.column == count) {
+                        A.changeEntry(i, count, aent.ent + bent.ent);
+                        a.moveNext();
+                        b.moveNext();
+                    } else if (aent.column != count && bent.column != count) {
+                        a.moveNext();
+                        b.moveNext();
+                    } else if (aent.column == count) {
+                        A.changeEntry(i, count, aent.ent);
+                        a.moveNext();
+                    } else {
+                        A.changeEntry(i, count, bent.ent);
+                        b.moveNext();
+                    }
+                }else if(a.index() != -1 && b.index() == -1){
+                    aent = (Point) a.get();
+                    if(aent.column == count) {
+                        A.changeEntry(i, count, aent.ent);
+                        a.moveNext();
+                    }
+                }else if(b.index() != -1 && a.index() == -1){
+                    bent = (Point) a.get();
+                    if(bent.column == count) {
+                        A.changeEntry(i, count, bent.ent);
+                        b.moveNext();
+                    }
                 }
                 count++;
             }
         }
         return A;
-
     }
     // returns a new Matrix that is the sum of this Matrix with M
     // pre: getSize()==M.getSize()
@@ -174,19 +212,33 @@ public class Matrix {
             Point bent = (Point) b.get();
             int count = 1;
             while(count <= size){
-                if(aent.equals(bent) && aent.column == count){
-                    A.changeEntry(i, count, aent.ent - bent.ent);
-                    a.moveNext();
-                    b.moveNext();
-                }else if(aent.column != count && bent.column != count){
-                    a.moveNext();
-                    b.moveNext();
-                }else if(aent.column == count){
-                    A.changeEntry(i, count, aent.ent);
-                    a.moveNext();
-                }else{
-                    A.changeEntry(i, count, -bent.ent);
-                    b.moveNext();
+                if(a.index() != -1 && b.index() != -1) {
+                    if (aent.equals(bent) && aent.column == count) {
+                        A.changeEntry(i, count, aent.ent - bent.ent);
+                        a.moveNext();
+                        b.moveNext();
+                    } else if (aent.column != count && bent.column != count) {
+                        a.moveNext();
+                        b.moveNext();
+                    } else if (aent.column == count) {
+                        A.changeEntry(i, count, aent.ent);
+                        a.moveNext();
+                    } else {
+                        A.changeEntry(i, count, -bent.ent);
+                        b.moveNext();
+                    }
+                }else if(a.index() != -1 && b.index() == -1){
+                    aent = (Point) a.get();
+                    if(aent.column == count) {
+                        A.changeEntry(i, count, aent.ent);
+                        a.moveNext();
+                    }
+                }else if(b.index() != -1 && a.index() == -1){
+                    bent = (Point) a.get();
+                    if(bent.column == count) {
+                        A.changeEntry(i, count, -bent.ent);
+                        b.moveNext();
+                    }
                 }
                 count++;
             }
