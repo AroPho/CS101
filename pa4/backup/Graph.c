27dc03b9012542de751c2648d20374b6d188bcc2
anguy200 Time Share <aaronn1999@gmail.com>
2019-05-29 13:11:16 -0700

    scripts

---
+++ pa4/backup/Graph.c
@@ -0,0 +1,205 @@
+//
+// Created by aaron on 5/23/2019.
+//
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<assert.h>
+#include"List.h"
+//#include"List.c"
+#include"Graph.h"
+
+#define INF -8
+#define NIL -9
+#define WHITE -1
+#define GRAY -2
+#define BLACK -3
+
+
+/*** Constructors-Destructors ***/
+
+typedef struct GraphObj{
+    List *neighbors;
+    int *parent;
+    int *color;
+    //int *queue;
+    int *distance;
+    int source;
+    int order;
+    int size;
+
+
+} GraphObj;
+
+typedef GraphObj *Graph;
+
+Graph newGraph(int n){
+    Graph G = malloc(sizeof(GraphObj));
+    assert(G != NULL);
+    G-> order = n;
+    G-> neighbors = malloc(n*sizeof(List));
+    G-> size = 0;
+    G-> source = NIL;
+    G-> parent = malloc((n+1)*sizeof(int));
+    G-> color = malloc((n+1)* sizeof(int));
+    //G-> queue = malloc(n* sizeof(int));
+    G-> distance = malloc((n+1)* sizeof(int));
+    for(int x = 0; x < n; x++){
+        G->neighbors[x] = newList();
+        G->parent[x] = NIL;
+        G->distance[x] = INF;
+        G->color[x] = WHITE;
+        //G->queue[x] = NIL;
+    }
+    return G;
+}
+void freeGraph(Graph* pG){
+    for(int x = 0; x < (*pG)->order; x++){
+        freeList(&((*pG)->neighbors[x]));
+    }
+    //free((*pG)->queue);
+    free((*pG)->parent);
+    free((*pG)->distance);
+    free((*pG)->color);
+    free((*pG)->neighbors);
+    //(*pG)->queue = NULL;
+    (*pG)->parent = NULL;
+    (*pG)->distance = NULL;
+    (*pG)->color = NULL;
+    (*pG)->neighbors = NULL;
+    free(*pG);
+    *pG = NULL;
+    return;
+}
+
+/*** Access functions ***/
+int getOrder(Graph G){
+    if( G != NULL){
+        return  G->order;
+    }
+    return 0;
+}
+int getSize(Graph G){
+    if(G != NULL){
+        return G->size;
+    }
+    return -1;
+}
+int getSource(Graph G){
+    if( G!= NULL){
+        return  G->source;
+    }
+}
+int getParent(Graph G, int u){
+    if( G != NULL){
+
+    }else if( u < 1 || u > G->order){
+
+    }
+    return G->parent[u];
+}
+int getDist(Graph G, int u){
+    if( G != NULL){
+
+    }else if( u < 1 || u > G->order){
+
+    }
+    return G->distance[u];
+}
+void getPath(List L, Graph G, int u){
+    if(G->source == u){
+        append(L, u);
+    }else if( G->source != NIL){
+        getPath(L, G, G->parent[u]);
+        append(L,u);
+    }else{
+
+    }
+}
+
+
+/*** Manipulation procedures ***/
+void makeNull(Graph G){
+    if(G != NULL){
+        for(int x = 0; x < G->order; x++){
+            clear(G->neighbors[x]);
+        }
+        G->size = 0;
+    }else{
+
+    }
+
+}
+void addEdge(Graph G, int u, int v){
+    if(G != NULL && u >= 1 && u <= getOrder(G)&& v >= 1 && v <= getOrder(G)){
+        addArc(G, u, v);
+        addArc(G, v, u);
+        G->size++;
+    }
+}
+void addArc(Graph G, int u, int v){
+    if(G != NULL && u >= 1 && u <= getOrder(G)&& v >= 1 && v <= getOrder(G)){
+        List A = G->neighbors[u];
+        /*if(A != NULL && length(A) == 0){
+            prepend(A, v);
+        }else {*/
+            moveFront(A);
+            while (index(A) != -1) {
+                if (index(A) > v) {
+                    insertBefore(A, v);
+                    break;
+                } else {
+                    moveNext(A);
+                }
+            }
+            if (index(A) == -1) {
+                append(A, v);
+            }
+        //}
+        G->size++;
+    }else{
+
+    }
+}
+
+
+/*** Other operations ***/
+void printGraph(FILE* out, Graph G){
+    if (out == NULL) {
+            exit(1);
+    } else if (G == NULL) {
+        exit(1);
+    }
+    for (int i = 1; i <= getOrder(G); i++) {
+        fprintf(out,"%d: ",i);
+        printList(out,G->neighbors[i]);
+    }
+}
+
+void BFS(Graph G, int s){
+    if(G != NULL && s >= 1 && s <= G->order){
+        G->source = s;
+        G->distance[s] = 0;
+        G->color[s] = GRAY;
+        List Q = newList();
+        List adj;
+        append(Q, s);
+        int current;
+        while(length(Q) != 0){
+            current = dequeue(Q);
+            adj = G->neighbors[current];
+            moveFront(adj);
+            while(index(adj) != -1){
+                if(G->color[index(adj)] == WHITE){
+                    append(Q, index(adj));
+                    G->parent[index(adj)] = current;
+                    G->color[index(adj)] = GRAY;
+                    G->distance[index(adj)] = G->distance[current] + 1;
+                }
+                moveNext(adj);
+            }
+            G->color[current] = BLACK;
+        }
+        freeList(&Q);
+    }
+}
---
+++ pa4/backup/List.c
@@ -0,0 +1,353 @@
+//PA2
+//CMPS 101 Tantalo Spring 2019
+//Aaron Nguyen
+//anguy200
+//1585632
+
+
+
+
+
+
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include "List.h"
+
+typedef struct NodeObj{
+  int data;
+  struct NodeObj *next;
+  struct NodeObj *prev;
+} NodeObj;
+
+typedef NodeObj *Node;
+
+Node newNode(int data){
+  Node N = malloc(sizeof(NodeObj));
+  assert(N != NULL);
+  N->data = data;
+  N->next = NULL;
+  return(N);
+}
+
+void freeNode(Node *pN){
+  if(pN != NULL && *pN != NULL){
+    free(*pN);
+    *pN = NULL;
+  }
+}
+
+typedef struct ListObj{
+  Node head;
+  Node tail;
+  Node cursor;
+  int index;
+  int numItems;
+} ListObj;
+
+typedef struct ListObj *List;
+
+List newList(void){
+  List L = malloc(sizeof(ListObj));
+  assert(L!=NULL);
+  L->head = NULL;
+  L->tail = NULL;
+  L->cursor = NULL;
+  L-> index = -1;
+  L-> numItems = 0;
+  return L;
+}
+
+void freeList(List *pL){
+  if(pL != NULL && *pL != NULL){
+    clear(*pL);
+    free(*pL);
+    *pL = NULL;
+  }
+}
+
+
+int length(List L){
+  if(L == NULL){
+    printf("This shit broke");
+  }
+  return L->numItems;
+}
+
+int index(List L){
+  return L->index;
+}
+
+int front(List L){
+  if(L->numItems > 0){
+    return L->head->data;
+  }else{
+    return -1;
+  }
+}
+
+int back(List L){
+  if(L->numItems > 0){
+    return L->tail->data;
+  }else{
+    return -1;
+  }
+}
+
+int get(List L){
+  if(L->numItems > 0 && L->index >= 0 && L->index < L->numItems){
+    return L->cursor->data;
+  }else{
+    return -1;
+  }
+}
+
+int equals(List A, List B){
+  if(A->numItems != B->numItems){
+    return 0;
+  }else{
+    Node tempA = A->head;
+    Node tempB = B->head;
+    for(int x = 0; x < A->numItems; x++){
+      if(tempA->data != tempB->data){
+        return 0;
+      }else{
+        tempA = tempA->next;
+        tempB = tempB->next;
+      }
+    }
+    return 1;
+  }
+}
+
+void clear(List L){
+  Node N = NULL;
+  if( L == NULL ){
+   perror("List is Null or empty");
+   exit(EXIT_FAILURE);
+  }
+  L->cursor = NULL;
+  L->index = -1;
+  while(L->numItems > 0){
+    N = L-> head;
+    L->head = L->head->next;
+    N->next = NULL;
+    freeNode(&N);
+    L->numItems--;
+  }
+}
+
+void moveFront(List L){
+  if(L->numItems == 0){
+    perror("List is Empty");
+    exit(EXIT_FAILURE);
+  }else{
+    L->cursor = L->head;
+    L->index = 0;
+  }
+}
+
+void moveBack(List L){
+  if(L-> numItems == 0){
+    perror("List is Empty");
+    exit(EXIT_FAILURE);
+  }else{
+    L->cursor = L->tail;
+    L->index = L->numItems - 1;
+  }
+}
+
+void movePrev(List L){
+  if(L->index != -1 && L->cursor != L->head){
+    L->cursor = L->cursor->prev;
+    L->index--;
+  }else if(L->cursor == L->head){
+    L->cursor = NULL;
+    L->index = -1;
+  }
+}
+
+void moveNext(List L){
+  if(L->cursor != NULL && L->cursor != L->tail){
+    L->cursor = L->cursor->next;
+    L->index++;
+  }else if(L->cursor == L->tail){
+    L->cursor = NULL;
+    L->index = -1;
+  }
+}
+
+
+void prepend(List L, int data){
+  //Node N = NULL;
+  //Node END = NULL;
+  if(L==NULL){
+    perror("List was not created properly");
+    exit(EXIT_FAILURE);
+  }
+  if(L->numItems == 0){
+    Node N = newNode(data);
+    L->head = L->tail = N;
+  }else{
+    Node temp = L->head;
+    L-> head = newNode(data);
+    L->head->next = temp;
+    temp->prev = L->head;
+    if(L->index != -1){
+      L->index++;
+    }
+    
+  }
+  L->numItems++;
+
+}
+
+void append(List L, int data){
+  if(L==NULL){
+    perror("List was not created correctly");
+    exit(EXIT_FAILURE);
+  }if(L->numItems == 0){
+    L->head =  newNode(data);
+    L->tail = L->head;
+  }else{
+    L->tail->next = newNode(data);
+    L->tail->next->prev = L->tail;
+    L->tail = L->tail->next;
+  }
+  L->numItems++;
+}
+
+void insertBefore(List L, int data){
+  if(L->numItems != 0 && L->index >= 0){
+    if(L->index == 0){
+      prepend(L, data);
+    }else{
+      Node A = L->cursor->prev;
+      Node temp = newNode(data);
+      A->next = temp;
+      temp -> prev = A;
+      temp -> next = L->cursor;
+      L->cursor->prev = temp;
+      L->index++;
+      L->numItems++;
+    }
+  }
+}
+
+void insertAfter(List L, int data){
+  if(L->numItems != 0 && L->numItems >= 0){
+    if(L->cursor == L->tail){
+      append( L, data);
+    }else{
+      Node A = L->cursor->next;
+      Node temp = newNode(data);
+      A->prev = temp;
+      temp->next = A;
+      temp->prev = L->cursor;
+      L->cursor->next = temp;
+      L->numItems++;
+    }
+  }
+}
+
+void deleteFront(List L){
+  if(L->numItems != 0){
+    Node temp = L->head;
+    L->head = L-> head->next;
+    L->head->prev = NULL;
+    freeNode(&temp);
+    L->numItems--;
+    if(L->index == 0){
+      L->index--;
+      L->cursor = NULL;
+    }else if(L-> index != -1){
+      L->index--;
+    }
+  }
+}
+
+void deleteBack(List L){
+  if(L->numItems > 0){
+    if(L->cursor == L->tail){
+      L->cursor = NULL;
+      L->index = -1;
+    }if(L->numItems == 1){
+      Node temp = L->tail;
+      freeNode(&temp);
+      L->tail = NULL;
+      L->head = NULL;
+      L->numItems--;
+    }else{
+      Node temp = L->tail;
+      L->tail = L->tail->prev;
+      L->tail->next = NULL;
+      freeNode(&temp);
+      L->numItems--;
+    }
+  }
+
+}
+
+void delete(List L){
+  if(L->index == 0 && L->numItems > 0){
+    deleteFront(L);
+  }else if(L-> index == L->numItems - 1 && L -> numItems > 0){
+    deleteBack(L);
+  }else if(L->index >= 0 && L->numItems !=0){
+    Node curprev = L->cursor->prev;
+    Node curnext = L->cursor->next;
+    curprev->next = curnext;
+    curnext->prev = curprev;
+    L->numItems--;
+    freeNode(&L->cursor);
+    L->index = -1;
+    L->cursor = NULL;
+  }
+
+}
+
+int dequeue(List L){
+  if(L != NULL){
+    moveFront(L);
+    int x = get(L);
+    deleteFront(L);
+    return x;
+  }
+  return -1;
+}
+
+void printNode(FILE *out, Node N){
+  fprintf(out, "%d ", N->data);
+}
+
+void printList(FILE* out, List L){
+  Node N = NULL;
+  if(L==NULL){
+    perror("List is not initialized");
+    exit(EXIT_FAILURE);
+  }
+  N = L->head;
+  while(N!=NULL){
+    fprintf(out, "%d ", N->data);
+    N = N->next;
+  }
+}
+
+List copyList(List L){
+  List tempList = newList();
+  Node temp = L->head;
+  while(temp != NULL){
+    append(tempList, temp->data);
+    temp = temp->next;
+  }
+  return tempList;
+}
+
+
+
+
+
+
+
