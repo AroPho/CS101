a8312f0652263d4a3bdb5f19886503075bcb4074
AroPho <aaronnguyen2009@yahoo.com>
2019-06-03 16:27:58 -0700

    Finding the problem

--- pa5/FindComponents.c
+++ pa5/FindComponents.c
@@ -72,7 +72,7 @@ int main (int argc, char* argv[]) {
     }
     int comp = 0;
     moveFront(S);
-    while(index(S) != -1 && comp <= scc){
+    while(index(S) != -1 && comp < scc){
         if(getParent(Gt, get(S)) == NIL){
             comp++;
         }
